3f875367c974890817da4db0ca286984c0158ad3
Anand3125 <georgian3125anand@gmail.com>
2024-10-01 19:30:44 +0530

    git add .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Container } from '@mui/material';
+import { Routes, Route } from "react-router-dom";
+import ResponsiveAppBar from './components/AppBar';
+
+import Footer from './components/Footer/Footer';
+import Contact from './modules/Contact';
+import HeroSection from './components/Hero/CarouselBig';
+import StatisticsSection from './components/StatCard/StatisticsSection';
+import Products from './components/Products';
+import Category from './components/Category';
+// import Home from './modules/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ResponsiveAppBar />
+      <HeroSection />
+      <Category />
+      <Products />
+      <StatisticsSection />
+     
+
+      <Container>
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+       
+          {/* <Route path="/" element={<HeroSection />} /> */}
+          {/* <Route path="/products/:id" element={<Product />} />  */}
+         
+          <Route path="/products" element={<Products />} />
+          {/* <Route path="/categories/:name" element={<CategoryProducts />} />  */}
+          {/* <Route path="cart" element={<Cart />} />
+          <Route path="/about" element={<HeroSection />} /> */}
+          {/* <Route path="/contact" element={<Contact />} /> */}
+             {/* <Route path="/Today" element={<Today />} /> */}
+          {/* <Route path="*" element={<div><h1>404 - Page Not Found</h1></div>} /> */}
+        </Routes>
+      </Container>
+     
+      {/* <StatisticsSection /> */}
+      <Footer />
+    </>
   );
 }
 
